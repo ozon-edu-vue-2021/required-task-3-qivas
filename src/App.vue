6e7a961b38095b257f8a845ca3fe0d4439e82bbd
apolyansky <aleksey.polyansky@pladform.ru>
2021-11-03 20:25:58 +0300

    feature-01 Have added seating employees plan

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map @select-table="selectTable" />
+      <SideMenu
+        :person="currentPerson"
+        :isUserOpened="isUserOpened"
+        @toggle-user="toggleBar"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -17,40 +21,67 @@ export default {
     Map,
     SideMenu,
   },
+  data() {
+    return {
+      currentPerson: {},
+      isUserOpened: false,
+    };
+  },
+  created() {
+    document.addEventListener("click", this.handleTableOutsideClick);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleTableOutsideClick);
+  },
+  methods: {
+    toggleBar(value) {
+      this.isUserOpened = value;
+    },
+    selectTable(person) {
+      this.$set(this.$data, "currentPerson", person);
+      this.toggleBar(true);
+    },
+    handleTableOutsideClick(event) {
+      const elem = event.target;
+      if (this.isUserOpened && !elem.classList.contains("wrapper-table")) {
+        this.toggleBar(false);
+      }
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
